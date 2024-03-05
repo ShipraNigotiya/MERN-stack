fd214df52a09b8d0c34f3086164ca7d024128faf
ShipraNigotiya <shipranigotiya@gmail.com>
2024-03-05 21:40:21 +0530

    add

--- Section 2/frontend/src/app/login/page.jsx
+++ Section 2/frontend/src/app/login/page.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Login = () => {
   return (
     <div> <h2> Login page</h2>
-    <button className='my-btn'> Login Submit</button></div>
+    <button className='my-btn'> Login Submit</button>
+    
+    <button className='btn btn-primary'>Nice</button>
+    </div>
+    
    
   )
 }
