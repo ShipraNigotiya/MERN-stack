c4a4044178f8f1b8fc62b04ae9b0defe38e975b2
ShipraNigotiya <shipranigotiya@gmail.com>
2024-03-09 18:53:47 +0530

    signup form handling

--- Section 2/frontend/src/app/login/page.jsx
+++ Section 2/frontend/src/app/login/page.jsx
@@ -1,18 +1,3 @@
-// import React from 'react'
-
-// const Login = () => {
-//   return (
-//     <div> <h2> Login page</h2>
-//     <button className='my-btn'> Login Submit</button>
-    
-//     <button className='btn btn-primary'>Nice</button>
-//     </div>
-    
-   
-//   )
-// }
-
-// export default Login
 'use client';
 import React from 'react';
 import Link from 'next/link';
