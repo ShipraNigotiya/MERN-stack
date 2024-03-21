470b11164814393727bd0ea4453833f302a16319
ShipraNigotiya <shipranigotiya@gmail.com>
2024-03-21 13:02:54 +0530

    backend

---
+++ Section 3/Backend/index.js
@@ -0,0 +1,18 @@
+// import express
+const express = require('express')
+
+// initialize express app
+const app = express();
+const port = 5000;
+
+app.get('/', (req,res) => {
+    res.send('response from express');
+});
+
+app.get('/add', (req,res) => {
+    res.send('add response');
+});
+
+
+
+app.listen(port, () => {console.log('server started');} );
